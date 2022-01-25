a1e6d33e577194852530bd0f7c7b771e7a6eb1a2
dev-shah-2204 <dgm82382@gmail.com>
2022-01-26 00:32:52 +0530

    Fix cog loading

--- utils/startup.py
+++ utils/startup.py
@@ -1,14 +1,12 @@
 import os
-import sys
 import json
-import discord
 
 from utils.colors import *
 
 
 def get_config():
 	if not os.path.isfile("config.json"):
-		sys.exit("'config.json' not found! Please add it and try again.")
+		exit("'config.json' not found! Please add it and try again.")
 
 	else:
 		with open("config.json") as file:
@@ -35,20 +33,22 @@ def load_commands(bot):
 
 
 def load_events(bot):
-	for file in os.listdir("cogs/events/"):
-		if file.endswith(".py"):
-			extension = file[:-3]
-			blacklisted_ext = []
-			blacklisted_ext = ["on_command_error"]
+	blacklisted_cogs = []
+	cogs = [
+		'on_command_completion',
+		'on_command_error',
+		'on_guild_join',
+		'on_guild_remove',
+		'on_ready'
+	]
 
-			if extension in blacklisted_ext:
-				print(f"{t_yellow}Skipped event: {extension}{t_white}")
-
-			else:
-				try:
-					bot.load_extension(f"cogs.events.{extension}")
-					print(f"Loaded event: {extension}")
-
-				except Exception as e:
-					exception = f"{type(e).__name__}: {e}"
-					print(f"{t_red}Failed to load event {extension}\n{exception}{t_white}")
+	for cog in cogs:
+		if cog in blacklisted_cogs:
+			print(f"{t_yellow}Skipped cog: {cog} {t_white}")
+		else:
+			try:
+				bot.load_extension(f"cogs.commands.{cog}")
+				print(f"Loaded cog: {cog}")
+			except Exception as e:
+				exception = f"{type(e).__name__}: {e}"
+				print(f"{t_red}Failed to load cog: cogs/commands/{cog}\n{exception} {t_white}")

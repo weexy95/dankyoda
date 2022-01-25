dd658838e01b37040f65303507ac0c87e8d8c488
cy-bug <69616790+cy-bug@users.noreply.github.com>
2022-01-25 17:23:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,90 +1,23 @@
 import json
 import sys
-
-from db import *
 import os
 import discord
+
+
 from discord.ext import commands
 from dotenv import load_dotenv
-
-"""
-+------------------------------------------------+
-| Defining some useful functions and variables.. |
-+------------------------------------------------+
-"""
-
-
-def get_config():
-	if not os.path.isfile("config.json"):
-		# sys.exit("'config.json' not found! Please add it and try again.")
-		print("FEK")
-	else:
-		with open("config.json") as file:
-			return json.load(file)
-
-
-def load_commands():
-	blacklisted_cogs = ["__pycache__"]
-	blacklisted_commands = []
-	print("---------------------------------")
-	for folder in os.listdir("cogs/commands/"):
-		if os.path.isdir(f"cogs/commands/{folder}"):
-			if folder in blacklisted_cogs:
-				print(f"--X Skipped Command Cog : {folder}")
-				continue
-			for command in os.listdir(f"cogs/commands/{folder}"):
-				if command.endswith(".py"):
-					cmd = command[:-3]
-				else:
-					continue
-				if cmd in blacklisted_commands:
-					print(f"--X Skipped Command : {cmd}")
-					pass
-				else:
-					try:
-						bot.load_extension(f"cogs.commands.{folder}.{cmd}")
-						print(f"--> Loaded command : '{cmd}'")
-					except discord.ExtensionAlreadyLoaded:
-						print("---------------------------------")
-						continue
-					except Exception as e:
-						exception = f"{type(e).__name__}: {e}"
-						print(f"--------------------------------------")
-						print(f"Failed to load Command : {cmd} from Cog {folder}..\n Traceback - {exception}")
-
-
-def load_events():
-	print("---------------------------------")
-	for file in os.listdir("cogs/events/"):
-		if file.endswith(".py"):
-			extension = file[:-3]
-			blacklisted_ext = []
-			blacklisted_ext = ["on_command_error"]
-			if extension in blacklisted_ext:
-				print(f"Skipped event : {extension}")
-			else:
-				try:
-					bot.load_extension(f"cogs.events.{extension}")
-					print(f"Loaded event  : {extension}")
-				except Exception as e:
-					exception = f"{type(e).__name__}: {e}"
-					print(f"Failed to load event {extension}\n	{exception}")
+from db import *
+from utils.startup import *
 
 
 load_dotenv()
-TOKEN = os.getenv("TOKEN")
-intents = discord.Intents.default()
-intents.members = True
+
+token = os.getenv("TOKEN")
+intents = discord.Intents.all()
 config = get_config()
 
-"""
-+--------------------------------+
-| Making the commands.Bot object |
-+--------------------------------+
-"""
-
 bot = commands.Bot(
-	command_prefix=config["bot_prefix"],
+	command_prefix=config['bot_prefix'],
 	intents=intents,
 	case_insensitive=True,
 	allowed_mentions=discord.AllowedMentions(everyone=False),
@@ -94,15 +27,17 @@ bot.remove_command("help")
 
 
 if __name__ == '__main__':
-	load_events()
-	load_commands()
+	load_events(bot)
+	load_commands(bot)
+
 	if not os.path.isfile("config.json"):
-		sys.exit("'config.json' not found! Please add it and try again.")
+		exit("'config.json' not found! Please add it and try again.")
 	else:
 		with open("config.json") as file:
 			config = json.load(file)
-	prefix = config["bot_prefix"]
+
+	bot.run(token)
 
 
-# Run the bot with the token
-bot.run(TOKEN)
+else:
+	raise RuntimeError("Make sure you're running the main.py file")

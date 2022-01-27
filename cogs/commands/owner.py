a91e25f0f49ec6a403843b5d7fa9319b3b1228a5
dev-shah-2204 <dgm82382@gmail.com>
2022-01-27 23:34:31 +0530

    Refactor

--- cogs/commands/owner.py
+++ cogs/commands/owner.py
@@ -9,7 +9,7 @@ from utils import economy, startup
 
 
 load_dotenv()
-heroku_key = os.getenv("heroku_key")
+heroku_key = os.getenv("HEROKU_API_KEY")
 urllib3.disable_warnings()
 
 
@@ -76,30 +76,36 @@ class Owner(commands.Cog):
 
     @commands.is_owner()
     @commands.command(name="botlog", aliases=["heroku-log"], usage="[lines]", help="Get bot logs - errors, warning, messages, etc.", description="Get the logs of the discord bot. If the number of lines is not defined then it defaults to 25 lines of most recent logs")
-    async def botlog(self, ctx, lines: int = 0):
-        if lines == 0:
-            lines = 25
-
+    async def botlog(self, ctx, lines: int = 25):
         logs = self.heroku_.get_app_log("dank-yoda", lines=lines)
         as_bytes = map(str.encode, logs)
         content = b"".join(as_bytes)
 
         if len(logs) < 1900:
-            await ctx.author.send(embed=discord.Embed(
-                description=f"Last {lines} lines of log by the bot from heroku app `dank-yoda`",
-                color=discord.Color.purple()
-            ), content=f'''```accesslog
-{logs}```''')
-        else:
-            await ctx.author.send(embed=discord.Embed(
-                description=f"Last {lines} lines of log by the bot from heroku app `dank-yoda`",
-                color=discord.Color.purple()
-            ), file=discord.File(BytesIO(content), "logs.log"))
+            await ctx.author.send(
+                embed=discord.Embed(
+                    description=f"Last {lines} lines of log by the bot from heroku app `dank-yoda`",
+                    color=discord.Color.purple()
+                ),
+                content=f'''```accesslog{logs}```'''
+            )
 
-        await ctx.send(embed=discord.Embed(
-            description="<:flux_check:934693030592655411> I have sent you a private message!",
-            color=discord.Color.brand_green()
-        ))
+        else:
+            await ctx.author.send(
+                embed=discord.Embed(
+                    description=f"Last {lines} lines of log by the bot from heroku app `dank-yoda`",
+                    color=discord.Color.purple()
+                ),
+                file=discord.File(BytesIO(content), "logs.log")
+            )
+
+        await ctx.send(
+            embed=discord.Embed(
+                description="<:flux_check:934693030592655411> I have sent you a private message!",
+                color=discord.Color.brand_green()
+            )
+        )
+
 
     @commands.is_owner()
     @commands.command(name="speak", aliases=["echo"], help="Send a normal message via the bot...")
@@ -109,13 +115,12 @@ class Owner(commands.Cog):
         else:
             return
 
+
     @commands.is_owner()
     @commands.command(name="reboot", aliases=['restart'], help="Stops the bot and starts it again...",)
     async def shutdown(self, ctx):
         async def view_timeout():
-            timeup_embed = discord.Embed(
-                description="Welp! fine not doing that"
-            )
+            timeup_embed = discord.Embed(description="Welp! fine not doing that")
             await choices.edit(embed=timeup_embed, view=None)
             await choices.delete(delay=5)
             return
@@ -124,10 +129,12 @@ class Owner(commands.Cog):
             if interaction.user.guild_permissions.administrator:
                 await interaction.message.edit(
                     embed=discord.Embed(description=f"Cancelled bot restart!", colour=discord.Colour.brand_green()),
-                    view=None)
+                    view=None
+                )
                 await choices.delete(delay=5)
                 view.stop()
                 return
+
             else:
                 pass
 
@@ -138,18 +145,25 @@ class Owner(commands.Cog):
                     color=0x42F56C
                 )
                 await ctx.send(embed=embed)
-                heroku_ = heroku3.from_key(os.getenv("heroku_key"))
-                flux_app = heroku_.apps()['dank-yoda']
-                flux_app.restart()
+
+                heroku = heroku3.from_key(os.getenv("HEROKU_API_KEY"))
+
+                dankyoda = heroku.apps()['dank-yoda']
+                dankyoda.restart()
+
             else:
                 pass
 
         view = discord.ui.View(timeout=15)
+
         ok_butt = discord.ui.Button(label="Restart!", style=discord.ButtonStyle.green)
         ok_butt.callback = ok_click
+
         view.add_item(ok_butt)
+
         cancel_butt = discord.ui.Button(label="Cancel!", style=discord.ButtonStyle.danger)
         cancel_butt.callback = cancel_click
+
         view.add_item(cancel_butt)
         view.on_timeout = view_timeout
 

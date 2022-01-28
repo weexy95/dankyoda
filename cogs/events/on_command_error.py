749b1fc35630cc2d86f0e38afde65bd8b39e84e2
cy-bug <shashwat5590@gmail.com>
2022-01-28 18:32:33 +0530

    added so bot sends a traceback file when error

--- cogs/events/on_command_error.py
+++ cogs/events/on_command_error.py
@@ -1,3 +1,4 @@
+import hashlib
 import json
 import random
 import traceback
@@ -7,171 +8,179 @@ import discord
 from discord.ext import commands
 
 
+async def gen_code(name: str):
+    return hashlib.sha256(name.encode('ascii', errors='ignore')).hexdigest()
+
+
 def usage(command):
-	args = []
+    args = []
 
-	for key, value in command.params.items():
-		if key not in ("self", "ctx"):
-			if "None" in str(value) or "No reason provided" in str(value):
-				args.append(f"[{key}]")
-			else:
-				args.append(f"<{key}>")
+    for key, value in command.params.items():
+        if key not in ("self", "ctx"):
+            if "None" in str(value) or "No reason provided" in str(value):
+                args.append(f"[{key}]")
+            else:
+                args.append(f"<{key}>")
 
-	args = " ".join(args)
+    args = " ".join(args)
 
-	return f"```{command} {args}```"
+    return f"```{command} {args}```"
 
 
 class ErrorHandling(commands.Cog):
-	def __init__(self, client):
-		self.client = client
-		with open("config.json", 'r') as config_file:
-			self.config = json.load(config_file)
-			config_file.close()
+    def __init__(self, client):
+        self.client = client
+        with open("config.json", 'r') as config_file:
+            self.config = json.load(config_file)
+            config_file.close()
 
-	@commands.Cog.listener()
-	async def on_command_error(self, ctx, error):
-		if isinstance(error, commands.MissingRequiredArgument):
-			em = discord.Embed(
-				title='Command incomplete!',
-				color=discord.Color.brand_red(),
-				description=f"This command is missing some arguements.. \n\n **Usage -** {usage(ctx.command)}"
-			)
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            em = discord.Embed(
+                title='Command incomplete!',
+                color=discord.Color.brand_red(),
+                description=f"This command is missing some arguements.. \n\n **Usage -** {usage(ctx.command)}"
+            )
 
-			await ctx.reply(embed=em, mention_author=False)
-			ctx.command.reset_cooldown(ctx)
-			return
-
-
-		elif isinstance(error, commands.MissingPermissions):
-			missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_permissions]
-
-			if len(missing) > 2:
-				permission = '{}, and {}'.format("**, **".join(missing[:-1]), missing[-1])
-			else:
-				permission = ' and '.join(missing)
-
-			em = discord.Embed(
-				title='Missing Permissions!',
-				description=f"You need the `{permission}` permission to do that",
-				color=discord.Color.brand_red()
-			)
-			await ctx.reply(embed=em, mention_author=False)
-			return
+            await ctx.reply(embed=em, mention_author=False)
+            ctx.command.reset_cooldown(ctx)
+            return
 
 
-		elif isinstance(error, commands.MemberNotFound):
-			not_found = [
-				"What are you talking about??\n\nNo such person exists on this server...",
-				"I searched through the deepest places of this server and still\nI couldn't find the person you mentioned",
-				"Welp, this person doesn't exist in this server."
-			]
-			em = discord.Embed(
-				description=random.choice(not_found),
-				color=discord.Color.brand_red())
+        elif isinstance(error, commands.MissingPermissions):
+            missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_permissions]
 
-			await ctx.reply(embed=em, mention_author=False)
-			ctx.command.reset_cooldown(ctx)
-			return
+            if len(missing) > 2:
+                permission = '{}, and {}'.format("**, **".join(missing[:-1]), missing[-1])
+            else:
+                permission = ' and '.join(missing)
 
-
-		elif isinstance(error, commands.BotMissingPermissions):
-			mp = error.missing_permissions[0]
-			mp = mp.title()
-			mp = mp.replace('_', ' ')
-
-			em = discord.Embed(
-				description=f"This should have worked but I have some permissions missing...\n\nMissing Permission - `{mp}`",
-				color=discord.Color.brand_red())
-
-			try:
-				await ctx.send(embed=em)
-				return
-			except discord.Forbidden:
-				await ctx.send(
-					f"I don't have the {mp} permission. F")  # In case the bot doesn't have embed links permission
-			return
+            em = discord.Embed(
+                title='Missing Permissions!',
+                description=f"You need the `{permission}` permission to do that",
+                color=discord.Color.brand_red()
+            )
+            await ctx.reply(embed=em, mention_author=False)
+            return
 
 
-		elif isinstance(error, commands.CommandOnCooldown):
-			mode = "second(s)"
-			if error.retry_after > 120:
-				error.retry_after = error.retry_after // 60
-				mode = "minute(s)"
+        elif isinstance(error, commands.MemberNotFound):
+            not_found = [
+                "What are you talking about??\n\nNo such person exists on this server...",
+                "I searched through the deepest places of this server and still\nI couldn't find the person you mentioned",
+                "Welp, this person doesn't exist in this server."
+            ]
+            em = discord.Embed(
+                description=random.choice(not_found),
+                color=discord.Color.brand_red())
 
-			if error.retry_after > 3600:
-				error.retry_after = error.retry_after // 3600
-				mode = "hour(s)"
-
-			em = discord.Embed(
-				title="Whoa there, hold your horses!",
-				description=f"The `{ctx.command}` command is on a cooldown, try again in **{error.retry_after:,.1f} {mode}**",
-				colour=discord.Color.brand_red()
-			)
-			await ctx.reply(embed=em, mention_author=False)
-			return
+            await ctx.reply(embed=em, mention_author=False)
+            ctx.command.reset_cooldown(ctx)
+            return
 
 
-		elif isinstance(error, commands.BadArgument):
-			em = discord.Embed(
-				title="Invalid arguments!", color=discord.Color.brand_red(),
-				description=f"I think you used the command wrong. For more info, try running: ```plz help {ctx.command}```"
-			)
-			await ctx.send(embed=em)
-			ctx.command.reset_cooldown(ctx)
-			return
+        elif isinstance(error, commands.BotMissingPermissions):
+            mp = error.missing_permissions[0]
+            mp = mp.title()
+            mp = mp.replace('_', ' ')
+
+            em = discord.Embed(
+                description=f"This should have worked but I have some permissions missing...\n\nMissing Permission - `{mp}`",
+                color=discord.Color.brand_red())
+
+            try:
+                await ctx.send(embed=em)
+                return
+            except discord.Forbidden:
+                await ctx.send(
+                    f"I don't have the {mp} permission. F")  # In case the bot doesn't have embed links permission
+            return
 
 
-		elif isinstance(error, commands.CommandNotFound):
-			return
+        elif isinstance(error, commands.CommandOnCooldown):
+            mode = "second(s)"
+            if error.retry_after > 120:
+                error.retry_after = error.retry_after // 60
+                mode = "minute(s)"
+
+            if error.retry_after > 3600:
+                error.retry_after = error.retry_after // 3600
+                mode = "hour(s)"
+
+            em = discord.Embed(
+                title="Whoa there, hold your horses!",
+                description=f"The `{ctx.command}` command is on a cooldown, try again in **{error.retry_after:,.1f} {mode}**",
+                colour=discord.Color.brand_red()
+            )
+            await ctx.reply(embed=em, mention_author=False)
+            return
 
 
-		elif isinstance(error, discord.Forbidden):
-			try:
-				em = discord.Embed(
-					title='Missing Permissions',
-					description="**Error code 403 Forbidden was raised. I can't do anything here...** \n\nContact my developers or join support server for help",
-					color=discord.Color.brand_red()
-				)
-				ctx.reply(embed=em, mention_author=False)
-				return
-			except discord.Forbidden:
-				await ctx.reply("I need the 'Embed Links' permission.")
-				return
+        elif isinstance(error, commands.BadArgument):
+            em = discord.Embed(
+                title="Invalid arguments!", color=discord.Color.brand_red(),
+                description=f"I think you used the command wrong. For more info, try running: ```plz help {ctx.command}```"
+            )
+            await ctx.send(embed=em)
+            ctx.command.reset_cooldown(ctx)
+            return
 
 
-		else:
-			await ctx.reply(embed=discord.Embed(
-				title="Something went Wrong!!",
-				description="An error occurred that I wasn't able to handle myself. This has been conveyed to my developer.",
-				color=discord.Color.brand_red()
-			))
-			channel = self.client.get_channel(self.config['err_channel'])
+        elif isinstance(error, commands.CommandNotFound):
+            return
 
-			em = discord.Embed(title='Error', color=0xFF3E3E)
 
-			em.add_field(name='Message:', value=f"<:reply:935420231185215509>`{ctx.message.content}`", inline=False)
-			em.add_field(name='Error:', value=f"```{error}```", inline=False)
-			em.add_field(name='Server:', value=f"<:reply:935420231185215509>guild-`{ctx.guild}`, channel-`#{ctx.channel}`", inline=False)
-			em.add_field(name='User:', value=f"{ctx.author.mention}", inline=False)
+        elif isinstance(error, discord.Forbidden):
+            try:
+                em = discord.Embed(
+                    title='Missing Permissions',
+                    description="**Error code 403 Forbidden was raised. I can't do anything here...** \n\nContact my developers or join support server for help",
+                    color=discord.Color.brand_red()
+                )
+                ctx.reply(embed=em, mention_author=False)
+                return
+            except discord.Forbidden:
+                await ctx.reply("I need the 'Embed Links' permission.")
+                return
 
-			view = discord.ui.View()
-			as_bytes = map(str.encode, str(traceback.format_exc()))
-			content = b"".join(as_bytes)
 
-			try:
-				link = await ctx.channel.create_invite(
-					temporary=True,
-					reason="Invite link to be sent to the bot developer.. for error report!"
-				)
-				invite_butt = discord.ui.Button(label="Visit server", url=link.url)
-				view.add_item(invite_butt)
-			except discord.HTTPException or discord.NotFound:
-				invite_butt = discord.ui.Button(label="Can't create invite", disabled=True)
-				view.add_item(invite_butt)
+        else:
+            if ctx.command.name.lower() == 'eval_fn':
+                return
 
-			await channel.send(embed=em, view=view, file=discord.File(BytesIO(content), "traceback.log"))
+            code = await gen_code(ctx.command.name.lower())
+            error = traceback.format_exception(etype=type(error), value=error, tb=error.__traceback__)
+            error = ''.join(error)
+
+            if error.endswith('Missing Permissions'):
+                try:
+                    return await ctx.send(
+                        'I am missing permissions inorder to run this command. I cannot identify the correct one.')
+                except discord.Forbidden:
+                    return
+
+            channel = self.bot.get_channel(800315954008948747)
+            if len(error) < 1850:
+                await channel.send(
+                    '**Error in the command {}**, Located from `{}` by user `{}`\n```\n'.format(ctx.command.name,
+                                                                                                ctx.guild.name,
+                                                                                                ctx.author) + error + '\n```')
+            else:
+                await channel.send(
+                    content='**Error in the command {}**, Located from `{}` by user `{}`'.format(ctx.command.name,
+                                                                                                 ctx.guild.name,
+                                                                                                 ctx.author),
+                    file=discord.File(fp=BytesIO(error.encode(errors='ignore')), filename='error.log')
+                )
+
+            try:
+                await ctx.send(
+                    '**An unknown error has occurred. It has been reported automatically!**\n**Your error code:** `{}`'.format(
+                        code))
+            except discord.Forbidden:
+                pass
 
 
 def setup(client):
-	client.add_cog(ErrorHandling(client))
+    client.add_cog(ErrorHandling(client))
